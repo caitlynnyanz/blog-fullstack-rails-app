1ff102ca6081d3213d636a5fb4d0a4d18c745142
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-11-27 09:30:30 -0700

    added users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
